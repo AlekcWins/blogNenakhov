c53dfbfd72e2b46218c0de9487208b78bb0e3679
AlexWinS <spvgg7fvy78c>
2020-09-04 21:16:35 +0500

    Добавьте файлы проекта.

---
+++ blogNenakhov/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogNenakhov.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: BlogController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
